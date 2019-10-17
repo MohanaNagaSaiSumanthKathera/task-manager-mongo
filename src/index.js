ff8f664401af05b663590e4dbd82ce1ccdda5e41
Sumanth Kathera <Sumanth.Kathera@arm.com>
2019-10-17 20:53:28 +0100

    added tasks,users get calls

--- src/index.js
+++ src/index.js
@@ -28,7 +28,49 @@ app.post('/tasks',(req,res)=>{
     }).catch((e)=>{
         res.status(400).send(e);
     })
-})
+});
+
+app.get('/users',(req,res)=>{
+    User.find({}).then((r)=>{
+        res.send(r);
+    }).catch((e)=>{
+        res.status(500).send(e);
+    });
+});
+
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id;
+
+    User.findById(_id).then((r)=>{
+        if(!r){
+          return res.status(404).send();
+        }
+        res.send(r);
+    }).catch((e)=>{
+        res.status(500).send(e);
+    });
+});
+
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then((r)=>{
+        res.send(r);
+    }).catch((e)=>{
+        res.status(500).send(e);
+    });
+});
+
+app.get('/tasks/:id',(req,res)=>{
+    const _id = req.params.id;
+
+    Task.findById(_id).then((r)=>{
+        if(!r){
+           return res.status(404).send();
+        }
+        res.send(r);
+    }).catch((e)=>{
+        res.status(500).send(e);
+    });
+});
 
 app.listen(port,()=>{
     console.log('Server is up on port'+ port);
